173155a9c5850820560b8216f7548447a97499f4
Wahyu Febrianto <febriantowahyu63@gmail.com>
2022-01-24 22:46:52 +0700

    fix: connection config & env

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -2,22 +2,13 @@ import { ConnectOptions, connect } from "mongoose";
 require("dotenv").config();
 export const connectingToMongoDB = async () => {
   try {
-    const mongoURI: string =
-      (process.env.NODE_ENV === "development"
-        ? process.env.MONGODB_URI_DEV
-        : "mongodb+srv://" +
-          process.env.MONGODB_USERNAME +
-          ":" +
-          process.env.MONGODB_PASSWORD +
-          process.env.MONGODB_URI_PROD) ||
-      "mongodb://localhost:27017/jaksel-translate";
+    const mongoURI: string = process.env.MONGODB_URI || "mongodb://localhost:27017/jaksel-translate";
     const options = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as ConnectOptions;
     await connect(mongoURI, options);
     console.log("MongoDB Connected...");
-    console.log(mongoURI);
   } catch (error) {
     console.log(error);
     process.exit(1);
