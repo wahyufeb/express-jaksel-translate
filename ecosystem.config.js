5a9359d75b73b7a81ab57a3ba2aa161cd83aea59
Wahyu Febrianto <febriantowahyu63@gmail.com>
2022-01-25 22:15:52 +0700

    fix: Pm2 Configurations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [{
-    name: "app",
+    name: "ApiJakselTranslate",
     script: "./build/index.js",
     env_development: {
-      NODE_ENV: "development",
+      NODE_ENV: "development"
     },
     env_production: {
       NODE_ENV: "production",
